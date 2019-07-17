57d2edc5c902ef5e999432d3f745904e87bda1ab
Bryan <bryan.dov.bergman@gmail.com>
2019-07-17 18:48:37 +0300

    Initial commit

---
+++ ForLoops.py
@@ -0,0 +1,23 @@
+_author_ = 'dev'
+
+
+# for i in range(1, 21):
+#     print("i is now {}".format(i))
+
+# number =  "9,223,372,036,854,775,807"
+# for i in range(0, len(number)):
+#     print(number[i])
+
+# number = "9,223,372,036,854,775,807"
+# cleanedNumber = ""
+# for i in range(0, len(number)):
+#     if number[i] in '0123456789':
+#         cleanedNumber = cleanedNumber + number[i]
+#         # print(number[i], end='')
+#
+# newNumber = int(cleanedNumber)
+# print("The number is {}".format(newNumber))
+
+for i in range(0,10):
+    print(i)
+
